9a9d2cf49c1e9db4d925c6dbe008d3336d35e46f
Eduardo García <eduardo.gl24@hotmail.com>
2022-07-29 04:24:46 -0500

    page create gol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateGoalComponent } from './pages/create-goal/create-goal.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'create-goal', component: CreateGoalComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateGoalComponent } from './pages/create-goal/create-goal.component';
 import { DetailsGoalComponent } from './pages/details-goal/details-goal.component';
 import { CreateRuleComponent } from './pages/create-rule/create-rule.component';
 import { SimulationComponent } from './pages/simulation/simulation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuComponent,
     HomeComponent,
     CreateGoalComponent,
     DetailsGoalComponent,
@@ -26,7 +25,8 @@ import { SimulationComponent } from './pages/simulation/simulation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
